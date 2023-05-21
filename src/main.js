e3f790426c600ada0a549e8adbde9633baabedec
Pavel <pvlwrk@gmail.com>
2023-05-21 13:38:51 +0300

    add settings hhost-apollo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import { createApolloClient } from '@nhost/apollo';
+import { NhostClient } from '@nhost/vue';
+import { DefaultApolloClient } from '@vue/apollo-composable';
+
+const nhost = new NhostClient({
+  subdomain: 'gnmhtflmezlvrmictnjt',
+  region: 'eu-central-1',
+});
+
+const apolloClient = createApolloClient({ nhost });
+
+createApp(App).provide(DefaultApolloClient, apolloClient).use(nhost).mount('#app');
