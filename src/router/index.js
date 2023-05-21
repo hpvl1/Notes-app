ea5c707048c007a5d0754b226f97d83a8264b681
Pavel <pvlwrk@gmail.com>
2023-05-21 14:12:57 +0300

    add router

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../pages/Home.vue'),
+      meta: {
+        protected: true,
+      }
+    },
+  ],
+});
+
+export default router;
